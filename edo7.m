19b64677755ef2142cc43e3ee59acdfcf08f2c32
Guilherme Crovador <157526022+cretss@users.noreply.github.com>
2025-01-21 09:02:24 -0300

    Create edo7.m

---
+++ edo7.m
@@ -0,0 +1,5 @@
+function dudt=edo7m(tinc2,u,m,k,C,incl1)
+dudt = [u(2); ((C*(-incl1)+k*(0.08-incl1*(tinc2-1.8))) - C*u(2) -
+k*(u(1)))/m];
+%a forca e a forca total de reacao da pista que esta agindo sobre o
+%carrinho
